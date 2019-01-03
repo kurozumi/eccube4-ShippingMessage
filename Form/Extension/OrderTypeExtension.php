65cc3eb7021d4d766ed3be3bd056788fe2f17fd4
akira <akira@akira-no-MacBook-Pro.local>
2019-01-04 01:53:37 +0900

    first commit

---
+++ Form/Extension/OrderTypeExtension.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Plugin\ShippingMessage\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Eccube\Form\Type\Shopping\OrderType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+/**
+ * ご注文手続きのお問い合わせを必須にする
+ *
+ * @author Akira Kurozumi <info@a-zumi.net>
+ */
+class OrderTypeExtension extends AbstractTypeExtension {
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $options = $builder->get('message')->getOptions();
+        $options['required'] = true;
+        $options['constraints'][] = new Assert\NotBlank;
+
+        $builder->add('message', TextareaType::class, $options);
+    }
+
+    public function getExtendedType()
+    {
+        return OrderType::class;
+    }
+
+}
